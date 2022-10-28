e0ebd887c1d8b3154c7a0a73e22a6fa42cc2af35
eunji <eunji1024k@gmail.com>
2022-10-28 17:56:41 +0900

    .

--- bntailer/js/main.js
+++ bntailer/js/main.js
@@ -38,21 +38,58 @@ $(document).ready(function(){
 	})
 
 	//페브릭의 이미지 스크롤 효과
+	//이미지가 스크롤 될 때 움직이는 효과
+	//움직이는 시작을 오브젝트가 화면에 나타나기 시작했을 때부터 스크롤 된 값을 계산해서 움직임 값으로 변환해야함
+	//  1. 스크롤 되는 값 -> $(window).scrollTop()
+	//  2. 오브젝트가 보이기 시작하는 스크롤 값
+	//  3. 오브젝트를 어떻게 움직일 것인지
+	// .fabric .bg img
+	// 		--> offset().top - 상단 맨 위에서부터 오브젝트까지의 거리값
+	// 			offset().top과 $(window).scrollTop 값이 같아지는 시기는 오브젝트가 화면 상단에 딱 붙었을 때입니다.
+	// 			--> 필요한건 오브젝트가 화면 하단에서 보이기 시작할 때
 
+	// 오브젝트가 화면 하단에서 나타나기 시작한 값은 오브젝트의 offset().top - 윈도우의 높이값만큼 스크롤 됐을 때 
+
+	let winH
+	let moveVal
+	let offTop
 	let scrolling
-	let moveTop
-	let objName = $('.fabric .bg2 img')
-	fabScroll()
-	$(window).scroll(function(){
-		fabScroll()
-	})
 
-	function fabScroll(){
-		//스크롤값을 요소의 위치값으로 변경해서 스타일을 적용
-		scrolling = $(window).scrollTop()
-		console.log(scrolling, 'scroll')
-		console.log(objName.offset().top)
-		moveTop = scrolling*0.1
-		//objName.css('transform','translate(0 , -'+moveTop+'px)')
+	// objMove = 실제움직일 오브젝트
+	// objParent = 움직일 오브젝트의 기준이 되는 요소
+	// moveDir = 스크롤방향
+	// moveRate = 움직일 속도와 비율
+	
+	objParallax($('.fabric .bg2'), $('.fabric .bg'), 'top', 0.2)
+	// objParallax($('.sns p'), $('.sns p'), 'left', 0.5)
+
+
+
+	function objParallax(objMove, objParent, moveDir, moveRate){ //오브젝트를 움직이는 애니메이션 단 한번 셋팅
+		objMove.css('transition', '1s')
+		moveAni(objMove, objParent, moveDir, moveRate)
+		$(window).scroll(function(){
+			moveAni(objMove, objParent, moveDir, moveRate)
+		})
+		$(window).resize(function(){
+			moveAni(objMove, objParent, moveDir, moveRate)
+		})
 	}
+
+	function moveAni(objMove, objParent, moveDir, moveRate){ //오브젝트를 실제 움직이는 함수 (반복실행)
+		winH = $(window).height()
+		offTop = objParent.offset().top
+		scrolling = $(window).scrollTop()
+		moveVal = (scrolling - offTop + winH)*moveRate
+		// console.log(winH,'window.height')
+		// console.log(offTop,'offTop')
+		// console.log(scrolling,'window.scrolling')
+		console.log(moveVal,'moveVal')
+		if (moveDir == 'left') {
+			objMove.css('transform','translateX(-'+moveVal+'px)')
+		} else {
+			objMove.css('transform','translateY(-'+moveVal+'px)')
+		}
+	}
+
 })
