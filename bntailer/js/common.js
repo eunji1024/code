c287227f516749abd4c260386a0a958a2f7b1ea0
eunji <eunji1024k@gmail.com>
2022-11-01 17:59:33 +0900

    .

--- bntailer/js/common.js
+++ bntailer/js/common.js
@@ -24,4 +24,29 @@ $(document).ready(function(){
             $('.header').removeClass('fixed')      
         }
     }
+
+    // .header .gnb .gnb_open를 클릭했을 때
+    // 1. 자기자신한테 mobile_open 클래스를 추가하거나 해제 (닫는 버튼과 여는 버튼 두가지 역할을 모두 함)
+    // 2. .header .gnb .gnb_open strong에 쓰여있는 글자를 '메뉴 열기', '메뉴 닫기'로 변경
+
+    // 메뉴가 열려있는 상태인지 메뉴가 닫혀있는 상태인지 구분을 해야 함
+
+    // 닫혀있으면 mobile_open 클래스를 추가해주고 '메뉴 닫기' 문구를 변경
+    // 열려있으면 mobile_open 클래스를 삭제, '메뉴 열기' 문구 변경
+    // mobile_open가 없으면 닫혀있는 상태 / 있으면 열린 상태
+
+    let gnbStu //메뉴의 열림 여부를 저장 (true 열린상태 / false 닫힌상태)
+    $('.header .gnb .gnb_open').on('click',function(){
+        gnbStu = $('.header .gnb').hasClass('mobile_open')
+        if (gnbStu == true) { //열린상태 > 닫기
+            $('.header .gnb').removeClass('mobile_open')
+            $('.header .gnb .gnb_open strong').text('메뉴열기')
+
+        } else { //닫힌상태 > 열기
+            $('.header .gnb').addClass('mobile_open')
+            $('.header .gnb .gnb_open strong').text('메뉴닫기')
+        }
+        
+    })
+
 })
